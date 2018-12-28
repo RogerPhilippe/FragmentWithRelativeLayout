95e90b13a6dbe52d595be4c8385a268dd6078c8a
rogerphilippe <roger.pereira@mc1.com.br>
2018-12-28 11:30:14 -0200

    Alteração itens list. Remoção imports desnecessários.

--- app/src/main/java/com/example/rpereira/fragmentwithrelativelayout/fragments/FragmentLayoutLeft.java
+++ app/src/main/java/com/example/rpereira/fragmentwithrelativelayout/fragments/FragmentLayoutLeft.java
@@ -2,8 +2,6 @@ package com.example.rpereira.fragmentwithrelativelayout.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
